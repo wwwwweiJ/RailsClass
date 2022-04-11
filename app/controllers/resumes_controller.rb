052c9cc0b4f738e5a6a4bde782bf79018b60e198
wei <leo2255leo2255@gmail.com>
2022-04-11 17:08:24 +0800

    resume list

--- app/controllers/resumes_controller.rb
+++ app/controllers/resumes_controller.rb
@@ -1,6 +1,28 @@
 class ResumesController < ApplicationController
   def index
+    @resumes = Resume.all
   end
+
   def new
+    @resume = Resume.new
+  end
+
+  def create
+    @resume = Resume.new(params_resume)
+
+    if @resume.save
+      redirect_to resumes_path , notice: "新增成功"
+    else
+      render :new
+    end
   end
 end
+
+
+
+private
+
+def params_resume
+  params.require(:resume).permit(:title , :content , :status)
+end
+
--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,7 @@ Rails.application.routes.draw do
 
   get "/resumes" , to: "resumes#index"
   get "/resumes/new" , to: "resumes#new"
+  post "/resumes" , to: "resumes#create"
 
   root 'pages#home'
 end
