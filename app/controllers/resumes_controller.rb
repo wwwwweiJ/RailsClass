b828f7b0a26c6c1dc6177d184d909ed159ced247
wei <leo2255leo2255@gmail.com>
2022-04-12 15:59:40 +0800

    finished

--- app/controllers/resumes_controller.rb
+++ app/controllers/resumes_controller.rb
@@ -1,4 +1,7 @@
 class ResumesController < ApplicationController
+
+before_action :resume_find , only: [:show , :destroy , :edit , :update]
+
   def index
     @resumes = Resume.all
   end
@@ -11,18 +14,41 @@ class ResumesController < ApplicationController
     @resume = Resume.new(params_resume)
 
     if @resume.save
-      redirect_to resumes_path , notice: "新增成功"
+      redirect_to root_path , notice: "新增成功"
     else
       render :new
     end
   end
-end
 
+  def show
+  end
+
+  def destroy 
+    @resume.destroy if @resume
+    redirect_to "/" , alert: "刪除成功"
+  end
+
+  def edit
+
+  end
+
+  def update
+    @resume.update(params_resume)
+    if @resume.save
+      redirect_to"/" , notice: "更新成功"
+    else
+      render :edit
+    end
+  end
 
 
 private
 
-def params_resume
-  params.require(:resume).permit(:title , :content , :status)
-end
+  def params_resume
+    params.require(:resume).permit(:title , :content , :status)
+  end
 
+  def resume_find
+    @resume = Resume.find(params[:id])
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,12 @@
 Rails.application.routes.draw do
 
+  root "resumes#index"
   get "/resumes" , to: "resumes#index"
   get "/resumes/new" , to: "resumes#new"
   post "/resumes" , to: "resumes#create"
-
-  root 'pages#home'
+  get "/resumes/:id" , to: "resumes#show" , as: "resume"
+  delete "/resumes/:id" , to: "resumes#destroy"
+  get "/resumes/:id/edit" , to: "resumes#edit" , as:"edit_resume"
+  patch "/resumes/:id" , to:"resumes#update"
+  
 end
